f7fe7dc6ca7002ad73f50a176ac873ca764478a6
Abdul Wajid <abdulwajidnv@gmail.com>
2023-05-30 11:36:42 +0530

    feat: configure appwrite

---
+++ api/appwrite.ts
@@ -0,0 +1,8 @@
+import { Client, Account } from "appwrite";
+
+export const client = new Client();
+export const account = new Account(client);
+
+client
+  .setEndpoint(process.env.NEXT_PUBLIC_API_ENDPOINT || "")
+  .setProject(process.env.NEXT_PUBLIC_PROJECT_ID || "");
