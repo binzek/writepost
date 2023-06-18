b152e1cd39b8b37db9b2504f503efc8939e37d3e
Abdul Wajid <abdulwajidnv@gmail.com>
2023-06-18 09:09:12 +0530

    fix: change footer year to dynamic

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,13 +13,16 @@ const poppins = Poppins({
 });
 
 const Footer: FC = () => {
+  // Get current year
+  let currentYear = new Date().getFullYear();
+
   return (
     <div className="mx-auto mb-2 w-[95%] lg:w-[98%]">
       <div className="h-[1px] w-full bg-clr-gray3" />
       <div
         className={`${poppins.className} mt-1 flex justify-between px-2 text-sm`}
       >
-        <p>&copy; WRITEPOST 2023</p>
+        <p>&copy; WRITEPOST {currentYear}</p>
         <div className="flex items-center gap-3">
           <Link href="https://buymeacoffee.com/wajidnv/" target="_blank">
             <HeartIcon dimension={22} strokeWidth={1.5} color="#F33A6A" />
