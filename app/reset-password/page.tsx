c04a4804585dd229f35bfe7eb2a1d862ed97dc9d
Abdul Wajid <abdulwajidnv@gmail.com>
2023-07-26 14:48:57 +0530

    feat: create password reset feature

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -2,11 +2,12 @@
 
 // Library imports
 import { FC, FormEvent, useContext, useState } from "react";
-import { redirect } from "next/navigation";
+import { redirect, useSearchParams, useRouter } from "next/navigation";
 import { Poppins, Raleway } from "next/font/google";
 
 // Local imports
 import ResetPasswordForm from "@/components/ResetPasswordForm";
+import ChangePasswordForm from "@/components/ChangePasswordForm";
 import { account } from "@/api/appwrite";
 import { AuthContext } from "@/context/AuthContext";
 
@@ -24,13 +25,58 @@ const ResetPasswordPage: FC = () => {
   // Get user's status
   const isUser = useContext(AuthContext);
 
+  // Get params initialization
+  const searchParams = useSearchParams();
+
+  // Router initialization
+  const router = useRouter();
+
   if (!isUser) {
     // States for email and password inputs
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // Function to reset password
+    const resetPassword = () => {
+      account
+        .createRecovery(email, "http://localhost:3000/reset-password")
+        .then(() => {
+          setEmail("");
+          alert("A password reset link has been sent to your inbox");
+          router.push("/signin");
+        })
+        .catch((error) => alert(error.message));
+    };
+
+    // Function to change password
+    const changePassword = () => {
+      account
+        .updateRecovery(
+          searchParams.get("userId") || "",
+          searchParams.get("secret") || "",
+          password,
+          password
+        )
+        .then(() => {
+          setPassword("");
+          alert(
+            "Your password has changed. You may sign in now with the new password"
+          );
+          window.close();
+        })
+        .catch((error) => alert(error.message));
+    };
 
     // Function to handle form submit
-    const handleSignIn = (e: FormEvent<HTMLFormElement>) => {
+    const handleReset = (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault();
+      resetPassword();
+    };
+
+    // Function to handle form submit
+    const handleChangePassword = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      changePassword();
     };
 
     return (
@@ -38,17 +84,34 @@ const ResetPasswordPage: FC = () => {
         className={`${poppins.className} mx-auto -mt-8 flex w-4/5 flex-1 flex-col items-center justify-center md:w-1/2 lg:w-1/3 xl:w-1/4`}
       >
         <h1 className={`${raleway.className} text-center text-2xl`}>
-          Reset Password
+          {searchParams.has("userId") &&
+          searchParams.has("secret") &&
+          searchParams.has("expire")
+            ? "Create New Password"
+            : "Reset Password"}
         </h1>
         <p className="mt-1 text-center text-sm font-light text-clr-gray4">
-          Enter your email below. We will send a password reset link to your
-          inbox
+          {searchParams.has("userId") &&
+          searchParams.has("secret") &&
+          searchParams.has("expire")
+            ? "To change you password, please enter your new password below"
+            : "Enter your email below. We will send a password reset link to your inbox"}
         </p>
-        <ResetPasswordForm
-          email={email}
-          setEmail={setEmail}
-          handleSubmit={handleSignIn}
-        />
+        {searchParams.has("userId") &&
+        searchParams.has("secret") &&
+        searchParams.has("expire") ? (
+          <ChangePasswordForm
+            handleSubmit={handleChangePassword}
+            password={password}
+            setPassword={setPassword}
+          />
+        ) : (
+          <ResetPasswordForm
+            email={email}
+            setEmail={setEmail}
+            handleSubmit={handleReset}
+          />
+        )}
       </div>
     );
   } else {
---
+++ components/ChangePasswordForm.tsx
@@ -0,0 +1,46 @@
+// Library imports
+import { FC, Dispatch, SetStateAction, FormEvent } from "react";
+
+// Sign up form props
+interface Props {
+  password: string;
+  setPassword: Dispatch<SetStateAction<string>>;
+  handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
+}
+
+const ChangePasswordForm: FC<Props> = ({
+  password,
+  setPassword,
+  handleSubmit,
+}) => {
+  return (
+    <form
+      action="#"
+      className="my-5 flex w-full flex-col gap-3"
+      onSubmit={handleSubmit}
+    >
+      <div className="flex flex-col">
+        <label htmlFor="password" className="text-sm">
+          Password
+        </label>
+        <input
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          id="password"
+          placeholder="******"
+          className="border border-clr-gray4 bg-transparent px-2 py-1 text-sm font-light outline-none placeholder:text-clr-gray3"
+        />
+      </div>
+      <button
+        type="submit"
+        className="mt-2 w-full bg-clr-black py-2 text-sm text-clr-gray1"
+      >
+        Change Password
+      </button>
+    </form>
+  );
+};
+
+export default ChangePasswordForm;
