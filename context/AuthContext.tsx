b2f255e060c2d08edd1653ece64f0a2bf0a92339
Abdul Wajid <abdulwajidnv@gmail.com>
2023-07-29 06:40:48 +0530

    fix: fix build errors

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,25 +1,29 @@
 "use client";
 
 // Library imports
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useState, FC, useEffect } from "react";
 
 // Local imports
 import { account } from "@/api/appwrite";
 
+interface Props {
+  children: ReactNode;
+}
+
 // Create context
 export const AuthContext = createContext(false);
 
-const AuthProvider = ({ children }: { children: ReactNode }) => {
+const AuthProvider: FC<Props> = ({ children }) => {
   // State for passing user's status
   const [isUser, setIsUser] = useState(false);
 
   // Fetch user's status on app load
-  window.onload = () => {
+  useEffect(() => {
     account
       .get()
       .then((user) => (user ? setIsUser(true) : setIsUser(false)))
       .catch(() => null);
-  };
+  }, []);
 
   // Return context provider with user's status
   return <AuthContext.Provider value={isUser}>{children}</AuthContext.Provider>;
